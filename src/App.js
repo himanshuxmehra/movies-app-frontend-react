74c1608098560409cde96f77715e95fba6305c51
Himanshu Mehra <himanshu24mehra@gmail.com>
2022-08-17 23:37:32 +0530

    Added Movie Details Page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   Route,
 } from "react-router-dom";
 import Detail from './components/Detail';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Header/>
         <Routes>
           <Route path="detail" element={<Detail/>} />
+          <Route path="login" element={<Login/>} />
           <Route path="/" element={<Home/>} />
         </Routes>
       </Router>
